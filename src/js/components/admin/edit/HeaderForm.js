8cf76da4cbaf820118bef0e16b1458440943bdec
Jochen Klar <mail@jochenklar.de>
2021-12-24 12:25:19 +0100

    Add addHeader, updateHeader and removeHeader to editForm

--- src/js/components/admin/edit/HeaderForm.js
+++ src/js/components/admin/edit/HeaderForm.js
@@ -7,36 +7,48 @@ class HeaderForm extends Component {
   }
 
   render() {
+    const { header, addHeader, updateHeader, removeHeader } = this.props
+
     return (
       <div className="mb-3">
         <div>
           <label>Table header</label>
         </div>
         {
-          Object.keys(this.props.header).map((key, index) => {
+          Object.keys(header).map((key, index) => {
+            const value = header[key]
+
             return (
               <div key={index} className="form-row align-items-center">
                 <div className="col-lg-2 mb-2">
                   <input
-                    readOnly
                     type="text"
                     className="form-control form-control-sm"
+                    onChange={event => updateHeader(event.target.value, value, key)}
                     value={key}
                   />
                 </div>
-                <div className="col-lg-10 mb-2">
+                <div className="col-lg-9 mb-2">
                   <input
                     type="text"
                     className="form-control form-control-sm"
-                    onChange={event => this.props.onChange(key, event.target.value)}
-                    value={this.props.header[key]}
+                    onChange={event => updateHeader(key, event.target.value)}
+                    value={value}
                   />
+                </div>
+                <div className="col-lg-1 mb-2">
+                  <button type="button" className="btn btn-danger btn-sm btn-block" onClick={() => removeHeader(key)}>Remove</button>
                 </div>
               </div>
             )
           })
         }
-        <small className="text-muted">The data you pick here will be added to the metadata of your converted file.</small>
+        <div className="mb-2">
+          <button type="button" className="btn btn-success btn-sm" onClick={addHeader}>Add header value</button>
+        </div>
+        <div>
+          <small className="text-muted">The data you pick here will be added to the metadata of your converted file.</small>
+        </div>
       </div>
     )
   }
