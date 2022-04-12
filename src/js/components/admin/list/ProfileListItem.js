be17215b7ec1d970f46fa0a955c84c03e713285d
Jochen Klar <mail@jochenklar.de>
2022-04-12 17:30:24 +0200

    Fix ProfileList

--- src/js/components/admin/list/ProfileListItem.js
+++ src/js/components/admin/list/ProfileListItem.js
@@ -6,16 +6,18 @@ class ProfileListItem extends Component {
     const { id, title, description, deleteProfile, editProfile, downloadProfile } = this.props
 
     return (
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        <div className="ms-2 me-auto">
-          <div className="font-weight-bold">{title}</div>
-          {description}
-        </div>
-        <div>
-          <code className="mr-2">{id}</code>
-          <span className="btn btn-success btn-sm mr-2" onClick={downloadProfile}>Download</span>
-          <span className="btn btn-primary btn-sm mr-2" onClick={editProfile}>Edit</span>
-          <span className="btn btn-danger btn-sm" onClick={deleteProfile}>Delete</span>
+      <li className="list-group-item">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="font-weight-bold">{title}</div>
+            {description}
+          </div>
+          <div className="col-lg-6">
+            <code className="mr-2">{id}</code>
+            <span className="btn btn-success btn-sm mr-2" onClick={downloadProfile}>Download</span>
+            <span className="btn btn-primary btn-sm mr-2" onClick={editProfile}>Edit</span>
+            <span className="btn btn-danger btn-sm" onClick={deleteProfile}>Delete</span>
+          </div>
         </div>
       </li>
     )
