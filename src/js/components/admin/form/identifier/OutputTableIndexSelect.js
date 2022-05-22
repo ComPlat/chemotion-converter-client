bf7f888c1f7cd63e8af5c876b99da5aa4aab22c7
Jochen Klar <mail@jochenklar.de>
2022-05-22 10:55:39 +0200

    Fix OutputTableIndexSelect

--- src/js/components/admin/form/identifier/OutputTableIndexSelect.js
+++ src/js/components/admin/form/identifier/OutputTableIndexSelect.js
@@ -4,9 +4,9 @@ const OutputTableIndexSelect = ({ index, identifier, tables, updateIdentifier })
   return (
     <React.Fragment>
       <select className="form-control form-control-sm" id={`outputTableIndexSelect${index}`}
-              value={identifier.outputTableIndex}
+              value={identifier.outputTableIndex === null ? '' : identifier.outputTableIndex}
               onChange={(event) => updateIdentifier(index, { outputTableIndex: parseInt(event.target.value, 10) })}>
-        <option value="">---</option>
+        <option value="">Add to all output tables</option>
         {
           tables.map((outputTable, outputTableIndex) => (
             <option key={outputTableIndex} value={outputTableIndex}>Output table #{outputTableIndex}</option>
