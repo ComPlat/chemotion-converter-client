29e2fd5714ad36482a6c3d7e1286e510151ae389
Jochen Klar <mail@jochenklar.de>
2022-10-13 10:59:42 +0200

    Fix IdentifierForm

--- src/js/components/admin/form/IdentifierForm.js
+++ src/js/components/admin/form/IdentifierForm.js
@@ -11,65 +11,68 @@ class IdentifierForm extends Component {
             tableMetadataOptions, inputTables, outputTables, dataset,
             addIdentifier, updateIdentifier, removeIdentifier,
             addIdentifierOperation, updateIdentifierOperation, removeIdentifierOperation } = this.props
-    const filteredIdentifiers = identifiers.filter(identifier => (identifier.type === type && identifier.optional == optional))
 
     const toggleIdentifier = (index) => {
       updateIdentifier(index, { show: !identifiers[index].show})
     }
 
+    const hasIdentifiers = identifiers.some(identifier => (identifier.type === type && identifier.optional == optional))
+
     return (
       <div className="mb-10">
         <label>{label}</label>
         {
-          filteredIdentifiers.length > 0 &&
+          hasIdentifiers &&
           <ul className="list-group mb-10">
             {
-              filteredIdentifiers.map((identifier, index) => {
-                if (identifier.show) {
-                  return (
-                    <li className="list-group-item" key={index}>
-                      <IdentifierHeader
-                        identifier={identifier}
-                        show={true}
-                        onToggle={() => toggleIdentifier(index)}
-                        onRemove={() => removeIdentifier(index)}
-                      />
-                      <IdentifierInput
-                        key={index}
-                        index={index}
-                        optional={optional}
-                        identifier={identifier}
-                        fileMetadataOptions={fileMetadataOptions}
-                        tableMetadataOptions={tableMetadataOptions}
-                        inputTables={inputTables}
-                        outputTables={outputTables}
-                        dataset={dataset}
-                        updateIdentifier={updateIdentifier}
-                        removeIdentifier={removeIdentifier}
-                        updateIdentifierOperation={updateIdentifierOperation}
-                        removeIdentifierOperation={removeIdentifierOperation}
-                      />
-                      {
-                        identifier.optional &&
-                        <div className="mb-10">
-                          <button type="button" className="btn btn-success btn-xs" onClick={event => addIdentifierOperation(index)}>
-                            Add scalar operation
-                          </button>
-                        </div>
-                      }
-                    </li>
-                  )
-                } else {
-                  return (
-                    <li key={index} className="list-group-item">
-                      <IdentifierHeader
-                        identifier={identifier}
-                        show={false}
-                        onToggle={() => toggleIdentifier(index)}
-                        onRemove={() => removeIdentifier(index)}
-                      />
-                    </li>
-                  )
+              identifiers.map((identifier, index) => {
+                if (identifier.type === type && identifier.optional == optional) {
+                  if (identifier.show) {
+                    return (
+                      <li className="list-group-item" key={index}>
+                        <IdentifierHeader
+                          identifier={identifier}
+                          show={true}
+                          onToggle={() => toggleIdentifier(index)}
+                          onRemove={() => removeIdentifier(index)}
+                        />
+                        <IdentifierInput
+                          key={index}
+                          index={index}
+                          optional={optional}
+                          identifier={identifier}
+                          fileMetadataOptions={fileMetadataOptions}
+                          tableMetadataOptions={tableMetadataOptions}
+                          inputTables={inputTables}
+                          outputTables={outputTables}
+                          dataset={dataset}
+                          updateIdentifier={updateIdentifier}
+                          removeIdentifier={removeIdentifier}
+                          updateIdentifierOperation={updateIdentifierOperation}
+                          removeIdentifierOperation={removeIdentifierOperation}
+                        />
+                        {
+                          identifier.optional &&
+                          <div className="mb-10">
+                            <button type="button" className="btn btn-success btn-xs" onClick={event => addIdentifierOperation(index)}>
+                              Add scalar operation
+                            </button>
+                          </div>
+                        }
+                      </li>
+                    )
+                  } else {
+                    return (
+                      <li key={index} className="list-group-item">
+                        <IdentifierHeader
+                          identifier={identifier}
+                          show={false}
+                          onToggle={() => toggleIdentifier(index)}
+                          onRemove={() => removeIdentifier(index)}
+                        />
+                      </li>
+                    )
+                  }
                 }
               })
             }
