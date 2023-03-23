089051efeee1e79ea7592a92e6e4b95988a871bd
Paggy <pei-chi.huang@kit.edu>
2023-03-23 09:59:26 +0100

    update copy webpack plugin lib

--- src/js/components/admin/list/ProfileListItem.js
+++ src/js/components/admin/list/ProfileListItem.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 class ProfileListItem extends Component {
 
   render () {
-    const { id, title, description, updateProfile, deleteProfile, downloadProfile } = this.props
+    const { id, title, description, updateProfile, deleteProfile, downloadProfile, isAdmin } = this.props
 
     return (
       <li className="list-group-item">
@@ -17,9 +17,9 @@ class ProfileListItem extends Component {
           </div>
           <div className="col-md-6 text-right">
             <code className="mr-10">{id}</code>
-            <span className="btn btn-success btn-sm mr-10" onClick={downloadProfile}>Download</span>
-            <span className="btn btn-primary btn-sm mr-10" onClick={updateProfile}>Edit</span>
-            <span className="btn btn-danger btn-sm" onClick={deleteProfile}>Delete</span>
+            <span className="btn btn-success btn-sm mr-10" disabled={!isAdmin} onClick={isAdmin ? downloadProfile : () => {}}>Download</span>
+            <span className="btn btn-primary btn-sm mr-10" disabled={!isAdmin} onClick={isAdmin ? updateProfile : () => {}}>Edit</span>
+            <span className="btn btn-danger btn-sm"  disabled={!isAdmin} onClick={isAdmin ? deleteProfile : () => {}}>Delete</span>
           </div>
         </div>
       </li>
@@ -34,7 +34,12 @@ ProfileListItem.propTypes = {
   description: PropTypes.string,
   updateProfile: PropTypes.func,
   deleteProfile: PropTypes.func,
-  downloadProfile: PropTypes.func
+  downloadProfile: PropTypes.func,
+  isAdmin: PropTypes.bool
 }
+
+ProfileListItem.defaultProps = {
+  isAdmin: false
+};
 
 export default ProfileListItem
