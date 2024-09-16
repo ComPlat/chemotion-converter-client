50af5220741ee6606a65aefc58c45ec1754c45ba
Luciano Maiwald <mail@luciano-maiwald.de>
2024-09-16 12:41:05 +0200

    Update IdentifierForm

--- src/js/components/admin/form/IdentifierForm.js
+++ src/js/components/admin/form/IdentifierForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import PropTypes from 'prop-types';
+import { Button, ListGroup } from 'react-bootstrap';
 
 import IdentifierInput from './IdentifierInput'
 import IdentifierHeader from './IdentifierHeader'
@@ -7,84 +8,76 @@ import IdentifierHeader from './IdentifierHeader'
 class IdentifierForm extends Component {
 
   render() {
-    const { label, type, optional, identifiers, fileMetadataOptions,
-            tableMetadataOptions, inputTables, outputTables, dataset,
-            addIdentifier, updateIdentifier, removeIdentifier,
-            addIdentifierOperation, updateIdentifierOperation, removeIdentifierOperation } = this.props
+    const {
+      label, type, optional, identifiers, fileMetadataOptions,
+      tableMetadataOptions, inputTables, outputTables, dataset,
+      addIdentifier, updateIdentifier, removeIdentifier,
+      addIdentifierOperation, updateIdentifierOperation, removeIdentifierOperation
+    } = this.props
 
     const toggleIdentifier = (index) => {
       updateIdentifier(index, { show: !identifiers[index].show})
     }
 
-    const hasIdentifiers = identifiers.some(identifier => (identifier.type === type && identifier.optional == optional))
+    const isRelevantIdentifier = (identifier) => (identifier.type === type && identifier.optional == optional)
+    const hasIdentifiers = identifiers.some(isRelevantIdentifier)
 
     return (
-      <div className="mb-10">
-        <label>{label}</label>
-        {
-          hasIdentifiers &&
-          <ul className="list-group mb-10">
-            {
-              identifiers.map((identifier, index) => {
-                if (identifier.type === type && identifier.optional == optional) {
-                  if (identifier.show) {
-                    return (
-                      <li className="list-group-item" key={index}>
-                        <IdentifierHeader
-                          identifier={identifier}
-                          show={true}
-                          onToggle={() => toggleIdentifier(index)}
-                          onRemove={() => removeIdentifier(index)}
-                        />
-                        <IdentifierInput
-                          key={index}
-                          index={index}
-                          optional={optional}
-                          identifier={identifier}
-                          fileMetadataOptions={fileMetadataOptions}
-                          tableMetadataOptions={tableMetadataOptions}
-                          inputTables={inputTables}
-                          outputTables={outputTables}
-                          dataset={dataset}
-                          updateIdentifier={updateIdentifier}
-                          removeIdentifier={removeIdentifier}
-                          updateIdentifierOperation={updateIdentifierOperation}
-                          removeIdentifierOperation={removeIdentifierOperation}
-                        />
-                        {
-                          identifier.optional &&
-                          <div className="mb-10">
-                            <button type="button" className="btn btn-success btn-xs" onClick={event => addIdentifierOperation(index)}>
-                              Add scalar operation
-                            </button>
-                          </div>
-                        }
-                      </li>
-                    )
-                  } else {
-                    return (
-                      <li key={index} className="list-group-item">
-                        <IdentifierHeader
-                          identifier={identifier}
-                          show={false}
-                          onToggle={() => toggleIdentifier(index)}
-                          onRemove={() => removeIdentifier(index)}
-                        />
-                      </li>
-                    )
-                  }
-                }
-              })
-            }
-          </ul>
-        }
-        <form>
-            <div className="form">
-              <button type="button" className="btn btn-success btn-xs" onClick={event => addIdentifier(type, optional)}>
-                {optional ? 'Add metadata' : 'Add Identifier'}
-              </button>
-            </div>
-        </form>
+      <div className="mb-3">
+        <div className="fw-bold">{label}</div>
+        {hasIdentifiers && (
+          <ListGroup>
+            {identifiers.map((identifier, index) => (
+              isRelevantIdentifier(identifier) && (
+                <ListGroup.Item key={index}>
+                  <IdentifierHeader
+                    identifier={identifier}
+                    show={identifier.show}
+                    onToggle={() => toggleIdentifier(index)}
+                    onRemove={() => removeIdentifier(index)}
+                  />
+                  {identifier.show && (
+                    <>
+                      <IdentifierInput
+                        index={index}
+                        optional={optional}
+                        identifier={identifier}
+                        fileMetadataOptions={fileMetadataOptions}
+                        tableMetadataOptions={tableMetadataOptions}
+                        inputTables={inputTables}
+                        outputTables={outputTables}
+                        dataset={dataset}
+                        updateIdentifier={updateIdentifier}
+                        removeIdentifier={removeIdentifier}
+                        updateIdentifierOperation={updateIdentifierOperation}
+                        removeIdentifierOperation={removeIdentifierOperation}
+                      />
+                      {identifier.optional && (
+                        <Button
+                          className="mt-1"
+                          variant="success"
+                          size="sm"
+                          onClick={() => addIdentifierOperation(index)}
+                        >
+                          Add scalar operation
+                        </Button>
+                      )}
+                    </>
+                  )}
+                </ListGroup.Item>
+              )
+            ))}
+          </ListGroup>
+        )}
+
+        <Button
+          className="mt-1"
+          variant="success"
+          size="sm"
+          onClick={() => addIdentifier(type, optional)}
+        >
+          {optional ? 'Add metadata' : 'Add Identifier'}
+        </Button>
       </div>
     )
   }
