1b4c6d917e081fe2d2f60e395a0242ed07bfb549
David Herrmann <david.herrmann@kit.edu>
2025-08-07 15:30:02 +0200

    new ontology button for create profile

--- src/js/components/admin/AdminApp.js
+++ src/js/components/admin/AdminApp.js
@@ -48,6 +48,8 @@ class AdminApp extends Component {
         this.importFile = this.importFile.bind(this);
 
         this.dispatchView = this.dispatchView.bind(this);
+
+        this.ontologyRef = React.createRef();
     }
 
     componentDidMount() {
@@ -217,11 +219,15 @@ class AdminApp extends Component {
     uploadFile() {
         const {selectedFile} = this.state
 
+        const ontology = this.ontologyRef.current.value;
+
+        console.log("chosen ontology", ontology);
+
         this.setState({
             isLoading: true
         })
 
-        ConverterApi.fetchTables(selectedFile)
+        ConverterApi.fetchTables(selectedFile, ontology)
             .then(data => {
                 if (data) {
                     const profile = {
@@ -298,6 +304,7 @@ class AdminApp extends Component {
                 <FileUploadForm
                     onFileChangeHandler={this.updateFile}
                     onSubmitFileHandler={handler}
+                    ontologyRef={this.ontologyRef}
                     errorMessage={errorMessage}
                     error={error}
                     isLoading={isLoading}
