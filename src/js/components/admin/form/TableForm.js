aea2666843c28f542fa7cb3ec52a480726768040
David <david.herrmann@kit.edu>
2026-05-28 09:04:05 +0200

    change spacing

--- src/js/components/admin/form/TableForm.js
+++ src/js/components/admin/form/TableForm.js
@@ -10,178 +10,178 @@ import SIunits from "./controllComponents/SIunits";
 
 
 function TableForm({
-  table, inputTables, inputColumns, options,
-  updateTable, updateHeader,
-  addOperation, updateOperation, updateOperationDescription, removeOperation,
-  fileMetadataOptions, tableMetadataOptions,
-  profile, setProfile, outputTableIndex
-}) {
-  const [showSiUnits, setShowSiUnits] = useState(false);
-  const [siUnitsContext, setSiUnitsContext] = useState(null);
-  const xy_units = {XUNITS: options.XUNITS, YUNITS: options.YUNITS}
+                       table, inputTables, inputColumns, options,
+                       updateTable, updateHeader,
+                       addOperation, updateOperation, updateOperationDescription, removeOperation,
+                       fileMetadataOptions, tableMetadataOptions,
+                       profile, setProfile, outputTableIndex
+                   }) {
+    const [showSiUnits, setShowSiUnits] = useState(false);
+    const [siUnitsContext, setSiUnitsContext] = useState(null);
+    const xy_units = {XUNITS: options.XUNITS, YUNITS: options.YUNITS}
 
-  const openSiUnits = (axis) => {
-    const inputColumn = axis === "X" ? table?.table?.xColumn : table?.table?.yColumn;
+    const openSiUnits = (axis) => {
+        const inputColumn = axis === "X" ? table?.table?.xColumn : table?.table?.yColumn;
 
-    setSiUnitsContext({
-      outputTableIndex,
-      axis,
-      inputColumn
-    });
-    setShowSiUnits(true);
-  };
+        setSiUnitsContext({
+            outputTableIndex,
+            axis,
+            inputColumn
+        });
+        setShowSiUnits(true);
+    };
 
-  const headerOptions = Object.keys(options).reduce(function (filtered, key) {
-    if (!(key in xy_units) && key !== 'rdf' ){
-       filtered[key] = options[key];
-    }
-    return filtered;
-  }, {});
+    const headerOptions = Object.keys(options).reduce(function (filtered, key) {
+        if (!(key in xy_units) && key !== 'rdf') {
+            filtered[key] = options[key];
+        }
+        return filtered;
+    }, {});
 
-  return (
-    <div>
-      <div className="fw-bold">
-        Table header
-      </div>
-
-      {Object.keys(headerOptions).map((optionKey, index) => (
-        <HeaderInput key={index} optionKey={optionKey} value={table.header[optionKey]}
-                     values={headerOptions[optionKey]} updateHeader={updateHeader}/>
-      ))}
-
-      {Object.keys(xy_units).map((optionKey, index) => (
-        <ExtendedHeaderInput key={index} optionKey={optionKey} value={table.header[optionKey]}
-                             values={xy_units[optionKey]} updateHeader={updateHeader}
-                             leftElement={(
-                               <Button
-                                 variant="outline-info"
-                                 type="button"
-                                 onClick={() => openSiUnits(optionKey === "XUNITS" ? "X" : "Y")}
-                               >
-                                 SI Units
-                               </Button>
-                             )}/>
-      ))}
-
-      {(table.header['DATA CLASS'] === 'NTUPLES') && (
-        <Form.Group as={Row}>
-          <Form.Label as={Col} sm={4}>NTUPLES PAGE HEADER</Form.Label>
-          <Col sm={8}>
-            <Form.Select
-              size="sm"
-              value={table.header['NTUPLES_PAGE_HEADER']}
-              onChange={event => {
-                const {value} = event.target;
-                updateHeader('NTUPLES_PAGE_HEADER', value);
-              }}
-            >
-              <option value="___+">Incrementing Page Index</option>
-              <option value="___TABLE_NAME">Input Table Name</option>
-              <option disabled>---METADATA---</option>
-              <>
-                {[...new Set(tableMetadataOptions.map(o => o.key))].map((option, optionIndex) => (
-                  <option key={optionIndex} data-idx={optionIndex} value={option}>{option}</option>
-                ))}
-              </>
-            </Form.Select>
-          </Col>
-        </Form.Group>
-      )}
-      <div className="mt-3 fw-bold">
-        Table columns
-      </div>
-
-      {(table.header['DATA CLASS'] === 'XYDATA') ? (
+    return (
         <div>
-          <div className="mb-2">
-            Which metadata should be used for the x-values?
-          </div>
-          {['FIRSTX', 'LASTX', 'DELTAX'].map((headerKey, index) => (
-            <TableIdentifier
-              key={index}
-              index={index + 1000}
-              headerKey={headerKey}
-              opitions={options}
-              table={table}
-              inputTables={inputTables}
-              updateHeader={updateHeader}
-              fileMetadataOptions={fileMetadataOptions}
-              tableMetadataOptions={tableMetadataOptions}
+            <div className="fw-bold">
+                Table header
+            </div>
+
+            {Object.keys(headerOptions).map((optionKey, index) => (
+                <HeaderInput key={index} optionKey={optionKey} value={table.header[optionKey]}
+                             values={headerOptions[optionKey]} updateHeader={updateHeader}/>
+            ))}
+
+            {Object.keys(xy_units).map((optionKey, index) => (
+                <ExtendedHeaderInput key={index} optionKey={optionKey} value={table.header[optionKey]}
+                                     values={xy_units[optionKey]} updateHeader={updateHeader}
+                                     leftElement={(
+                                         <Button
+                                             variant="outline-info"
+                                             type="button"
+                                             onClick={() => openSiUnits(optionKey === "XUNITS" ? "X" : "Y")}
+                                         >
+                                             SI Units
+                                         </Button>
+                                     )}/>
+            ))}
+
+            {(table.header['DATA CLASS'] === 'NTUPLES') && (
+                <Form.Group as={Row}>
+                    <Form.Label as={Col} sm={4}>NTUPLES PAGE HEADER</Form.Label>
+                    <Col sm={8}>
+                        <Form.Select
+                            size="sm"
+                            value={table.header['NTUPLES_PAGE_HEADER']}
+                            onChange={event => {
+                                const {value} = event.target;
+                                updateHeader('NTUPLES_PAGE_HEADER', value);
+                            }}
+                        >
+                            <option value="___+">Incrementing Page Index</option>
+                            <option value="___TABLE_NAME">Input Table Name</option>
+                            <option disabled>---METADATA---</option>
+                            <>
+                                {[...new Set(tableMetadataOptions.map(o => o.key))].map((option, optionIndex) => (
+                                    <option key={optionIndex} data-idx={optionIndex} value={option}>{option}</option>
+                                ))}
+                            </>
+                        </Form.Select>
+                    </Col>
+                </Form.Group>
+            )}
+            <div className="mt-3 fw-bold">
+                Table columns
+            </div>
+
+            {(table.header['DATA CLASS'] === 'XYDATA') ? (
+                <div>
+                    <div className="mb-2">
+                        Which metadata should be used for the x-values?
+                    </div>
+                    {['FIRSTX', 'LASTX', 'DELTAX'].map((headerKey, index) => (
+                        <TableIdentifier
+                            key={index}
+                            index={index + 1000}
+                            headerKey={headerKey}
+                            opitions={options}
+                            table={table}
+                            inputTables={inputTables}
+                            updateHeader={updateHeader}
+                            fileMetadataOptions={fileMetadataOptions}
+                            tableMetadataOptions={tableMetadataOptions}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <TableColumn
+                    table={table.table}
+                    label="Which column should be used as x-values?"
+                    columnKey="xColumn"
+                    operationsKey="xOperations"
+                    inputColumns={inputColumns}
+                    updateTable={updateTable}
+                    addOperation={addOperation}
+                    updateOperation={updateOperation}
+                    updateOperationDescription={updateOperationDescription}
+                    removeOperation={removeOperation}
+                    tableMetadataOptions={tableMetadataOptions}
+                    inputTables={inputTables}
+                />
+            )}
+
+            <TableColumn
+                table={table.table}
+                label="Which column should be used as y-values?"
+                columnKey="yColumn"
+                operationsKey="yOperations"
+                inputColumns={inputColumns}
+                updateTable={updateTable}
+                addOperation={addOperation}
+                updateOperation={updateOperation}
+                updateOperationDescription={updateOperationDescription}
+                removeOperation={removeOperation}
+                tableMetadataOptions={tableMetadataOptions}
+                inputTables={inputTables}
             />
-          ))}
+
+            <small className="text-muted">The data you pick will determine which table columns are going to
+                converted.</small>
+
+            <Offcanvas
+                show={showSiUnits}
+                onHide={() => setShowSiUnits(false)}
+                placement="start"
+                style={{width: "min(1100px, 50vw)"}}
+            >
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>SI Units</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <SIunits
+                        profile={profile}
+                        setProfile={setProfile}
+                        defaultAssignmentContext={siUnitsContext}
+                    />
+                </Offcanvas.Body>
+            </Offcanvas>
         </div>
-      ) : (
-        <TableColumn
-          table={table.table}
-          label="Which column should be used as x-values?"
-          columnKey="xColumn"
-          operationsKey="xOperations"
-          inputColumns={inputColumns}
-          updateTable={updateTable}
-          addOperation={addOperation}
-          updateOperation={updateOperation}
-          updateOperationDescription={updateOperationDescription}
-          removeOperation={removeOperation}
-          tableMetadataOptions={tableMetadataOptions}
-          inputTables={inputTables}
-        />
-      )}
-
-      <TableColumn
-        table={table.table}
-        label="Which column should be used as y-values?"
-        columnKey="yColumn"
-        operationsKey="yOperations"
-        inputColumns={inputColumns}
-        updateTable={updateTable}
-        addOperation={addOperation}
-        updateOperation={updateOperation}
-        updateOperationDescription={updateOperationDescription}
-        removeOperation={removeOperation}
-        tableMetadataOptions={tableMetadataOptions}
-        inputTables={inputTables}
-      />
-
-      <small className="text-muted">The data you pick will determine which table columns are going to
-        converted.</small>
-
-      <Offcanvas
-        show={showSiUnits}
-        onHide={() => setShowSiUnits(false)}
-        placement="start"
-        style={{width: "min(1100px, 50vw)"}}
-      >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>SI Units</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <SIunits
-            profile={profile}
-            setProfile={setProfile}
-            defaultAssignmentContext={siUnitsContext}
-          />
-        </Offcanvas.Body>
-      </Offcanvas>
-    </div>
-  )
+    )
 }
 
 TableForm.propTypes = {
-  table: PropTypes.object,
-  inputTables: PropTypes.array,
-  inputColumns: PropTypes.array,
-  options: PropTypes.object,
-  updateTable: PropTypes.func,
-  updateHeader: PropTypes.func,
-  updateOperationDescription: PropTypes.func,
-  addOperation: PropTypes.func,
-  updateOperation: PropTypes.func,
-  removeOperation: PropTypes.func,
-  fileMetadataOptions: PropTypes.array,
-  tableMetadataOptions: PropTypes.array,
-  profile: PropTypes.object,
-  setProfile: PropTypes.func,
-  outputTableIndex: PropTypes.number
+    table: PropTypes.object,
+    inputTables: PropTypes.array,
+    inputColumns: PropTypes.array,
+    options: PropTypes.object,
+    updateTable: PropTypes.func,
+    updateHeader: PropTypes.func,
+    updateOperationDescription: PropTypes.func,
+    addOperation: PropTypes.func,
+    updateOperation: PropTypes.func,
+    removeOperation: PropTypes.func,
+    fileMetadataOptions: PropTypes.array,
+    tableMetadataOptions: PropTypes.array,
+    profile: PropTypes.object,
+    setProfile: PropTypes.func,
+    outputTableIndex: PropTypes.number
 }
 
 export default TableForm
